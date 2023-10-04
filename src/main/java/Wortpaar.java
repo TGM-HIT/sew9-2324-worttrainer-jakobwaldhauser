ade151c5c0266ea930d25d83c0f48c5209afda64
jakobwaldhauser <jwaldhauser@student.tgm.ac.at>
2023-10-04 17:07:02 +0200

    Wortpaar Klasse

---
+++ src/main/java/Wortpaar.java
@@ -0,0 +1,25 @@
+public class Wortpaar {
+    private String wort;
+    private String url;
+
+    public Wortpaar(String wort, String url){
+        this.wort = wort;
+        this.url = url;
+    }
+
+    public String getWort(){
+        return this.wort;
+    }
+
+    public String getURL(){
+        return this.url;
+    }
+
+    public void setWort(String wort){
+        this.wort = wort;
+    }
+
+    public void setURL(String url){
+        this.url = url;
+    }
+}
