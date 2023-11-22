c97a06f54e3776cd4858c5396a3b43bc3c038bed
jakobwaldhauser <jwaldhauser@student.tgm.ac.at>
2023-11-22 16:20:43 +0100

    Issue bearbeitet (mehr Wörter hinzugefügt)

--- src/main/java/SpeichernJSON.java
+++ src/main/java/SpeichernJSON.java
@@ -44,6 +44,11 @@ public class SpeichernJSON implements Speichern{
         list1.add(new Wortpaar("Igel", "https://th.bing.com/th/id/OIP.TfSik2avbGBkvZVtOGlmrAHaE8?pid=ImgDet&rs=1"));
         list1.add(new Wortpaar("Tiger", "https://th.bing.com/th/id/OIP.MccTUyTQmGwGUDtaeO8A3gHaHa?pid=ImgDet&rs=1"));
         list1.add(new Wortpaar("Nashorn", "https://img.fotocommunity.com/nashorn-portrait-4122ea52-84e8-419b-9c32-dc2b8cddeaf2.jpg?width=1000"));
+        list1.add(new Wortpaar("Adler", "https://jooinn.com/images/adler-4.jpg"));
+        list1.add(new Wortpaar("Ratte", "https://www.slantmagazine.com/wp-content/uploads/2018/11/ratatouille-1000x600.jpg"));
+        list1.add(new Wortpaar("Loewe", "https://images5.alphacoders.com/665/thumb-1920-665846.jpg"));
+        list1.add(new Wortpaar("Giraffe", "https://th.bing.com/th/id/R.964d43d8632d0dc4a22ccd02c68e41a6?rik=eOh0DTlQ21C2kA&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2ff%2ff9%2fGiraffe_Portrait%2c_Woburn_Safari_Park.jpg&ehk=wZaeO8iK9Cbm1prBj1sqbBvAb5oHrI0mASKFPv56XIQ%3d&risl=&pid=ImgRaw&r=0"));
+        list1.add(new Wortpaar("Auto", "https://th.bing.com/th/id/OIP.uszUxJ8AmvyMasfHABIEewHaFD?rs=1&pid=ImgDetMain"));
 
         RechtschreibTrainer rt1 = new RechtschreibTrainer(list1);
 
