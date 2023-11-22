9ed34f700e6a76b0ea8f07d57dfc8b32bef204e6
jakobwaldhauser <jwaldhauser@student.tgm.ac.at>
2023-11-22 15:23:06 +0100

    Speichermechanismus Verbesserung: bei keinem vorhandenem File wird nun wirklich ein Worttrainer mit einigen Wörtern erstellt. (bei letztem Commit hierzu wurde es doch nicht fertig implementiert)

--- src/main/java/SpeichernJSON.java
+++ src/main/java/SpeichernJSON.java
@@ -32,10 +32,23 @@ public class SpeichernJSON implements Speichern{
         try (FileReader reader = new FileReader(file)) {
             return gson.fromJson(reader, RechtschreibTrainer.class);
         } catch (IOException e) {
-            e.printStackTrace();
-            return new RechtschreibTrainer(new ArrayList<Wortpaar>());
+            return loadStandard();
 
         }
+    }
+
+    public RechtschreibTrainer loadStandard(){
+        ArrayList<Wortpaar> list1 = new ArrayList<>();
+        list1.add(new Wortpaar("Hund", "https://th.bing.com/th/id/R.542611eacb5c3972de6a1fecc74de38b?rik=ycZGLnItV7s%2bAQ&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fJgYQiAp.jpg&ehk=fFlqdbVQY9ouK0xw%2bBZMS3nGESR1GRHJOcL1c4M0t5M%3d&risl=&pid=ImgRaw&r=0"));
+        list1.add(new Wortpaar("Katze", "https://th.bing.com/th/id/OIP.M9UpJPCBRKl9U2JKDqJPhgHaEK?pid=ImgDet&rs=1"));
+        list1.add(new Wortpaar("Igel", "https://th.bing.com/th/id/OIP.TfSik2avbGBkvZVtOGlmrAHaE8?pid=ImgDet&rs=1"));
+        list1.add(new Wortpaar("Tiger", "https://th.bing.com/th/id/OIP.MccTUyTQmGwGUDtaeO8A3gHaHa?pid=ImgDet&rs=1"));
+        list1.add(new Wortpaar("Nashorn", "https://img.fotocommunity.com/nashorn-portrait-4122ea52-84e8-419b-9c32-dc2b8cddeaf2.jpg?width=1000"));
+
+        RechtschreibTrainer rt1 = new RechtschreibTrainer(list1);
+
+        rt1.wortAuswaehlen();
+        return rt1;
     }
 
 }
